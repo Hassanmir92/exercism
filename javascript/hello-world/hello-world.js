90695a68faf8357e2bf1144f27a02f52ea89b89f
Hassanmir92 <hassanmir92@gmail.com>
2015-06-25 14:20:57 +0100

    added roman numeral and hello world js

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+  
+};
+
+module.exports = HelloWorld;
