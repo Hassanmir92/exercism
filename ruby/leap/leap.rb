3eb203cfebd4c10ed1242f0f6778623f379bb7f0
Hassanmir92 <hassanmir92@gmail.com>
2015-06-29 15:25:07 +0100

    shorter solution for leap

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,11 +1,7 @@
 class Year
 
   def self.leap?(year)
-    if year % 400 == 0 || (year % 4 == 0 && !(year % 100 == 0))
-      true
-    else
-      false
-    end
+    year % 400 == 0 || (year % 4 == 0 && !(year % 100 == 0))
   end
 
 end
