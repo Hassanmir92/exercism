575ebbb6d623f35a14f6cc757893e98701716ae2
Hassanmir92 <hassanmir92@gmail.com>
2015-07-28 14:06:43 +0100

    cleaned up the code for difference of squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,28 +1,20 @@
 class Squares
   attr_reader :number
 
-    def initialize(number)
-      @number=number
-    end
+  def initialize(number)
+    @number=number
+  end
 
-    def square_of_sums
-      sum = 0
-      (1..number).each do |i|
-        sum += i
-      end
-      sum**2
-    end
+  def square_of_sums
+    (1..number).reduce { |sum, i| sum += i } ** 2
+  end
 
-    def sum_of_squares
-      sum = 0
-      (1..number).each do |i|
-        sum += i**2
-      end
-      sum
-    end
+  def sum_of_squares
+    (1..number).reduce { |sum, i| sum += i**2 }
+  end
 
-    def difference
-      square_of_sums - sum_of_squares
-    end
+  def difference
+    square_of_sums - sum_of_squares
+  end
 end
 
