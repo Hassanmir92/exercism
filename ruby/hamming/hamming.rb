17d83a7dbbd1a44220307169ca03a2eecf493038
Hassanmir92 <hassanmir92@gmail.com>
2015-07-28 13:23:37 +0100

    updated hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,12 +2,10 @@ class Hamming
   VERSION = 1
 
   def self.compute(a, b)
-    if a.length == b.length
-      (0...a.length).count do |i|
-        a[i] != b[i]
-      end
-    else
-      raise ArgumentError
+    raise ArgumentError if a.length != b.length
+    
+    (0...a.length).count do |i|
+      a[i] != b[i]
     end
   end
 
