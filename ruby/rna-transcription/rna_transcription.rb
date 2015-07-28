c0758dd7047f00f296255d11707286bf65e4d14b
Hassanmir92 <hassanmir92@gmail.com>
2015-07-28 14:56:53 +0100

    rna transcription more readable code

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,18 +1,13 @@
 class Complement
 
   def self.of_dna(strand)
-    if ['U'].include? strand
-      raise ArgumentError
-    else
-      strand.tr *['CGTA', 'GCAU']
-    end  
+    raise ArgumentError if ['U'].include? strand
+    strand.tr *['CGTA', 'GCAU']
   end
 
   def self.of_rna(strand)
-    if ['T'].include? strand
-      raise ArgumentError
-    else
-      strand.tr *['GCAU', 'CGTA']
-    end
+    raise ArgumentError if ['T'].include? strand
+    strand.tr *['GCAU', 'CGTA']
   end
+  
 end
