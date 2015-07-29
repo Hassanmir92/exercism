43ea1ae82ce24e4f45aca56692230a16b7ba5d18
Hassanmir92 <hassanmir92@gmail.com>
2015-07-29 14:20:54 +0100

    updating folder

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -1,40 +1,13 @@
 class Meetup
 
   attr_reader :year, :month
+  
   def initialize(month, year)
     @year = year
     @month = month
-    @first = Date.new(year, month, 1)
-    @eighth = Date.new(year, month, 8)
-    @thirteenth = Date.new(year, month, 13)
-    @fifteenth = Date.new(year, month, 15)
-    @twenty_second = Date.new(year, month, 22)
-    @last = Date.new(year, month, -1)
   end
 
   def day(weekday, schedule)
-   case schedule
-   when :teenth then
-     @thirteenth + remainder(weekday)
-   when :first then
-     @first
-   when :second then
-     @eighth
-   when :third then
-     @fifteenth
-   when :fourth then
-     @twenty_second
-   when :last then
-     @last
-   end  
- end
-
- def remainder(weekday)
-  if weekday == :monday
-    (10 % month) * 3
-  elsif weekday == :tuesday
-    (10 % month) * 6
-  end
  end
 
 end
