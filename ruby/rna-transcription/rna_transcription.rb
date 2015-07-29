7c7e18f7dd1f6a4b4df641a867abbc0997f01c0c
Hassanmir92 <hassanmir92@gmail.com>
2015-07-29 11:46:30 +0100

    3 new tests passed for rna-transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,13 +1,26 @@
 class Complement
+  VERSION = 1
+  VALID_DNA = ['C', 'G', 'T', 'A']
+  VALID_RNA = ['C', 'G', 'U', 'A']
 
   def self.of_dna(strand)
-    raise ArgumentError if ['U'].include? strand
+    raise ArgumentError if valid_dna?(strand)
     strand.tr *['CGTA', 'GCAU']
   end
 
   def self.of_rna(strand)
-    raise ArgumentError if ['T'].include? strand
+    raise ArgumentError if valid_rna?(strand)
     strand.tr *['GCAU', 'CGTA']
+  end
+
+  def self.valid_dna?(strand)
+    valid = VALID_DNA - strand.split('')
+    !(valid.length < 4)
+  end
+
+  def self.valid_rna?(strand)
+    valid = VALID_RNA - strand.split('')
+    !(valid.length < 4)
   end
   
 end
--- ruby/rna-transcription/rna_transcription_test.rb
+++ ruby/rna-transcription/rna_transcription_test.rb
@@ -51,4 +51,18 @@ class ComplementTest < Minitest::Test
   def test_rna_raises_argument_error
     assert_raises(ArgumentError) { Complement.of_rna('T') }
   end
-end
+
+  def test_rna_raises_argument_error_on_completely_invalid_input
+    assert_raises(ArgumentError) { Complement.of_rna('XXX') }
+  end
+
+  def tes_dna_raises_argument_error_on_completely_invalid_input
+    assert_raises(ArgumentError) { Complement.of_dna('XXX') }
+  end
+
+    # This test is for the sake of people providing feedback, so they
+    # know which version of the exercise you are solving.
+    def test_bookkeeping
+      assert_equal 1, Complement::VERSION
+    end
+  end
