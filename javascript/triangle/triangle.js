2f8102191809a47db0cd62449febbce1260df87b
Hassanmir92 <hassanmir92@gmail.com>
2015-12-11 11:54:26 +0000

    triangle test done js

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,43 @@
+var Triangle = function(h,w,l){
+  this.height = h;
+  this.width  = w;
+  this.length = l;
+  this.sides = [h,w,l];
+};
+
+Triangle.prototype.kind = function(){
+  if (this.illegal(this.sides)) {
+    throw new Error;
+  };
+
+  if(this.equilateral(this.sides)) return "equilateral";
+  if(this.isosceles(this.sides)) return "isosceles";
+  if(this.scalene(this.sides)) return "scalene";
+};
+
+Triangle.prototype.illegal = function(triangle){
+  var largestSide = Math.max.apply(Math, triangle);
+  var twoSmallerSides = [];
+  for (var i = 0; i < triangle.length; i++) {
+    if(triangle[i]<=0) return true;
+    if(triangle[i]!==largestSide){
+      twoSmallerSides.push(triangle[i]);
+    };
+  };
+  if(twoSmallerSides[0]+twoSmallerSides[1]<largestSide) return true;
+  return false
+};
+
+Triangle.prototype.equilateral = function(triangle){
+  return triangle[0] === triangle[1] && triangle[1] === triangle[2]
+};
+
+Triangle.prototype.isosceles = function(triangle){
+  return triangle[0] === triangle[1] || triangle[1] === triangle[2] || triangle[0] === triangle[2]
+};
+
+Triangle.prototype.scalene = function(triangle){
+  return triangle[0] !== triangle[1] !== triangle[2]
+};
+
+module.exports = Triangle;
